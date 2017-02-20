e6d6eebc3ee41a91c0bdddd861bb1c87964248d7
Jiangming1399 <Jiangming1399@163.com>
2017-02-20 23:01:53 +0800

    增加部分文件

---
+++ bootloader/pca10028/dual_bank_ble_s110/arm5_no_packs/RTE/_nrf51422_xxac/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'dfu_dual_bank_ble_s110_pca10028' 
+ * Target:  'nrf51422_xxac' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ pca10028/s110/arm5_no_packs/RTE/_flash_s110_nrf51_8.0.0_softdevice/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_hids_keyboard_s110_pca10028' 
+ * Target:  'flash_s110_nrf51_8.0.0_softdevice' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+#endif /* RTE_COMPONENTS_H */
