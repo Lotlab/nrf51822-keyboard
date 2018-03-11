f89bd509086311ae39fa897539d5a79469a2765d
Jiangming1399 <jiangming1399@163.com>
2018-03-11 21:32:36 +0800

    微调代码位置

--- main/hids.c
+++ main/hids.c
@@ -10,7 +10,6 @@
 #include "led.h"
 #include "main.h"
 
-
 #define OUTPUT_REPORT_INDEX 0                   /**< Index of Output Report. */
 #define OUTPUT_REPORT_MAX_LEN 1                 /**< Maximum length of Output Report. */
 #define INPUT_REPORT_KEYS_INDEX 0               /**< Index of Input Report. */
@@ -21,6 +20,54 @@
 #define MAX_BUFFER_ENTRIES 5 /**< Number of elements that can be enqueued */
 
 #define BASE_USB_HID_SPEC_VERSION 0x0101 /**< Version number of base USB HID Specification implemented by this application. */
+
+static uint8_t report_map_data[] =
+{
+		0x05, 0x01, // Usage Page (Generic Desktop)
+		0x09, 0x06, // Usage (Keyboard)
+		0xA1, 0x01, // Collection (Application)
+		0x05, 0x07, //     Usage Page (Key Codes)
+		0x19, 0xe0, //     Usage Minimum (224)
+		0x29, 0xe7, //     Usage Maximum (231)
+		0x15, 0x00, //     Logical Minimum (0)
+		0x25, 0x01, //     Logical Maximum (1)
+		0x75, 0x01, //     Report Size (1)
+		0x95, 0x08, //     Report Count (8)
+		0x81, 0x02, //     Input (Data, Variable, Absolute)
+
+		0x95, 0x01, //     Report Count (1)
+		0x75, 0x08, //     Report Size (8)
+		0x81, 0x01, //     Input (Constant) reserved byte(1)
+
+		0x95, 0x05, //     Report Count (5)
+		0x75, 0x01, //     Report Size (1)
+		0x05, 0x08, //     Usage Page (Page# for LEDs)
+		0x19, 0x01, //     Usage Minimum (1)
+		0x29, 0x05, //     Usage Maximum (5)
+		0x91, 0x02, //     Output (Data, Variable, Absolute), Led report
+		0x95, 0x01, //     Report Count (1)
+		0x75, 0x03, //     Report Size (3)
+		0x91, 0x01, //     Output (Data, Variable, Absolute), Led report padding
+
+		0x95, 0x06, //     Report Count (6)
+		0x75, 0x08, //     Report Size (8)
+		0x15, 0x00, //     Logical Minimum (0)
+		0x25, 0x65, //     Logical Maximum (255)
+		0x05, 0x07, //     Usage Page (Key codes)
+		0x19, 0x00, //     Usage Minimum (0)
+		0x29, 0x65, //     Usage Maximum (101)
+		0x81, 0x00, //     Input (Data, Array) Key array(6 bytes)
+
+		0x09, 0x05,       //     Usage (Vendor Defined)
+		0x15, 0x00,       //     Logical Minimum (0)
+		0x26, 0xFF, 0x00, //     Logical Maximum (255)
+		0x75, 0x08,       //     Report Count (2)
+		0x95, 0x02,       //     Report Size (8 bit)
+		0xB1, 0x02,       //     Feature (Data, Variable, Absolute)
+
+		0xC0 // End Collection (Application)
+};
+
 
 static ble_hids_t m_hids;                                /**< Structure used to identify the HID service. */
 static bool m_in_boot_mode = false;                      /**< Current protocol mode. */
