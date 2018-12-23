9ccda83f3b5e3f801f2263920ba39658091f481b
Jiangming1399 <jiangming1399@163.com>
2018-12-23 15:34:41 +0800

    Remove files for project SoftDevice, to fix compile error #4.

---
+++ main/project/arm5_no_packs/RTE/_Generic/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_hids_keyboard_s110_pca10028' 
+ * Target:  'Generic' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+#endif /* RTE_COMPONENTS_H */
